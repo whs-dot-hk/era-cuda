617deb620a1d1f7cd6842dc56f15eaae53a53deb
whs <hswongac@gmail.com>
2024-01-03 12:33:57 +0800

    Fix

--- cudart-sys/build.rs
+++ cudart-sys/build.rs
@@ -257,9 +257,8 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    fs::write(
-        PathBuf::from("src").join("bindings.rs"),
-        bindings.to_string(),
-    )
-    .expect("Couldn't write bindings!");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
